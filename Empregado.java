415ff1725fa85738f3415c62ccbb7efa34bca08b
Nicholas Cunha Saturnino da Silva - 821223990 <821223990@ulife.com.br>
2024-02-28 20:20:02 -0300

    Segundo Commit. Calculo de salario realizado

--- Empregado.java
+++ Empregado.java
@@ -1,7 +1,20 @@
 public class Empregado{
     private String nome;
-    private int idade;
+    private int idade, tipo;
+    private double salario, comissao, bonus;
     
+    public double calculaSalario (){
+        if (tipo == 1){
+            return salario;
+        }else if (tipo == 2){
+        //comissão varia de 0 a 1, é um percentual sobre o salário
+            return salario + salario * comissao;
+        }else if (tipo == 3){
+            return salario + bonus;
+        }else{
+            return 0.;
+        }
+    }
     public String getNome (){
     return this.nome;
     }
